3583448d70e76ebbabf2154a0387d8c4eb63a79f
Stas-ef <stasef@gmail.com>
2026-01-11 21:57:29 +0500

    Begin

---
+++ src/main/java/org/skypro/AppForExam/AppForExamApplication.java
@@ -0,0 +1,14 @@
+package org.skypro.AppForExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForExamApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForExamApplication.class, args);
+	}
+
+}
