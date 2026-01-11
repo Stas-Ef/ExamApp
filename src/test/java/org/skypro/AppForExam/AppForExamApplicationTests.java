a0978574876835ef40d3825ad1a97e19c5f9c16d
Stas-ef <stasef@gmail.com>
2026-01-11 22:28:51 +0500

    Revert "Finish_JavaCORE"
    This reverts commit 1b6f0a10147a31ee19dbf4f3602ba91739bbf1b3.

---
+++ src/test/java/org/skypro/AppForExam/AppForExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.AppForExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppForExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
